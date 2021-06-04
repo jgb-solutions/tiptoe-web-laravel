f1ab59f2e54d9c350170881d95b53367e305f3cb
Poliard Djimmy <poliarddjimmy@gmail.com>
2021-06-04 03:04:31 -0400

    Updated migration for follower class

---
+++ database/migrations/2021_06_04_064950_create_followers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFollowersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('followers', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->bigInteger('modele_id');
+            $table->timestamps();
+
+            $table->foreign('user_id', 'modele_id')->references('id')->on('users', 'modeles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('followers');
+    }
+}
