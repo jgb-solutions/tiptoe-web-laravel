a429ca8c22e81dea08e2c14d7063414c2b980a38
Poliard Djimmy <poliarddjimmy@gmail.com>
2021-06-04 03:03:27 -0400

    Add followers relationship for modele class

--- app/Models/Modele.php
+++ app/Models/Modele.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 
 class Modele extends Model
@@ -28,5 +29,10 @@ class Modele extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function followers(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, Follower::class,'modele_id', 'user_id');
     }
 }
