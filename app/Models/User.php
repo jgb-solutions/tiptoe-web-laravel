4966460fa11ad43487466db7ed02396a4b218454
Poliard Djimmy <poliarddjimmy@gmail.com>
2021-06-29 01:31:25 -0400

    Use laravel sanctum, hash password on created

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,11 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Passport\HasApiTokens;
+// use Laravel\Passport\HasApiTokens;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Facades\Hash;
 
 
 class User extends Authenticatable
@@ -66,11 +67,11 @@ class User extends Authenticatable
         parent::boot();
 
         self::creating(function ($data) {
-
-        if (!$data['avatar']) {
-            $data['avatar'] = "https://www.pngitem.com/pimgs/m/150-1503945_transparent-user-png-default-user-image-png-png.png";
-        }
+            $data['password'] = Hash::make($data['password']);
             
+            if (!$data['avatar']) {
+                $data['avatar'] = "https://www.pngitem.com/pimgs/m/150-1503945_transparent-user-png-default-user-image-png-png.png";
+            }
         });
 
         self::created(function ($user) {
