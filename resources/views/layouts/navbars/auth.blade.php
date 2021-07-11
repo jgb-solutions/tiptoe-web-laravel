09ebae171931ba91666878d51f1ab2d5ad426ee0
Poliard Djimmy <poliarddjimmy@gmail.com>
2021-07-11 13:51:08 -0400

    Setup dashboard

---
+++ resources/views/layouts/navbars/auth.blade.php
@@ -0,0 +1,78 @@
+<div class="sidebar" data-color="white" data-active-color="danger">
+    <div class="logo">
+        <a href="http://www.creative-tim.com" class="simple-text logo-normal">
+            <div class="logo-image">
+                <img src="/images/logo.png" style="height: 30px">
+            </div>
+        </a>
+    </div>
+    <div class="sidebar-wrapper" style="overflow-x:hidden">
+        <ul class="nav">
+            <li class="{{ $elementActive == 'dashboard' ? 'active' : '' }}">
+                <a href="{{ route('page.index', 'dashboard') }}">
+                    <i class="nc-icon nc-bank"></i>
+                    <p>{{ __('Dashboard') }}</p>
+                </a>
+            </li>
+            <li class="{{ $elementActive == 'user' || $elementActive == 'profile' ? 'active' : '' }}">
+                <a data-toggle="collapse" aria-expanded="false" href="#usersManagement">
+                    <i class="nc-icon nc-single-02"></i>
+                    <p>
+                        {{ __('Users Management') }}
+                        <b class="caret mt-1"></b>
+                    </p>
+                </a>
+                <div class="collapse {{ $elementActive == 'user' || $elementActive == 'profile' ? 'show' : 'hide'  }}"
+                    id="usersManagement">
+                    <ul class="nav">
+                        <li class="{{ $elementActive == 'profile' ? 'active' : '' }}">
+                            <a href="{{ route('profile.edit') }}">
+                                <span class="sidebar-mini-icon">{{ __('M') }}</span>
+                                <span class="sidebar-normal">{{ __(' Model') }}</span>
+                            </a>
+                        </li>
+                        <li class="{{ $elementActive == 'user' ? 'active' : '' }}">
+                            <a href="{{ route('page.index', 'user') }}">
+                                <span class="sidebar-mini-icon">{{ __('C') }}</span>
+                                <span class="sidebar-normal">{{ __(' Consumer') }}</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="{{ $elementActive == 'icons' ? 'active' : '' }}">
+                <a href="{{ route('page.index', 'icons') }}">
+                    <i class="nc-icon nc-diamond"></i>
+                    <p>{{ __('Icons') }}</p>
+                </a>
+            </li>
+            <li class="{{ $elementActive == 'map' ? 'active' : '' }}">
+                <a href="{{ route('page.index', 'map') }}">
+                    <i class="nc-icon nc-pin-3"></i>
+                    <p>{{ __('Maps') }}</p>
+                </a>
+            </li>
+            <li class="{{ $elementActive == 'notifications' ? 'active' : '' }}">
+                <a href="{{ route('page.index', 'notifications') }}">
+                    <i class="nc-icon nc-bell-55"></i>
+                    <p>{{ __('Notifications') }}</p>
+                </a>
+            </li>
+            <li class="{{ $elementActive == 'tables' ? 'active' : '' }}">
+                <a href="{{ route('page.index', 'tables') }}">
+                    <i class="nc-icon nc-tile-56"></i>
+                    <p>{{ __('Table List') }}</p>
+                </a>
+            </li>
+            <li class="{{ $elementActive == 'typography' ? 'active' : '' }}">
+                <a href="{{ route('page.index', 'typography') }}">
+                    <i class="nc-icon nc-caps-small"></i>
+                    <p>{{ __('Typography') }}</p>
+                </a>
+            </li>
+            <!-- <li class="active-pro {{ $elementActive == 'upgrade' ? 'active' : '' }}">
+                sdwd
+            </li> -->
+        </ul>
+    </div>
+</div>
---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,67 @@
+@extends('layouts.app', [
+'class' => '',
+'elementActive' => 'user'
+])
+
+@section('content')
+<div class="content">
+    <div class="container-fluid mt--7">
+        <div class="row">
+            <div class="col">
+                <div class="card shadow">
+                    <div class="card-header border-0">
+                        <div class="row align-items-center">
+                            <div class="col-8">
+                                <h3 class="mb-0">Users</h3>
+                            </div>
+                            <!-- <div class="col-4 text-right">
+                                <a href="#" class="btn btn-sm btn-primary">Add user</a>
+                            </div> -->
+                        </div>
+                    </div>
+
+                    <div class="col-12">
+                    </div>
+
+                    <div class="table-responsive">
+                        <table class="table align-items-center table-flush">
+                            <thead class="thead-light">
+                                <tr>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">Creation Date</th>
+                                    <th scope="col"></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Admin Admin</td>
+                                    <td>
+                                        <a href="mailto:admin@paper.com">admin@paper.com</a>
+                                    </td>
+                                    <td>25/02/2020 11:37</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="card-footer py-4">
+                        <nav class="d-flex justify-content-end" aria-label="...">
+
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+@endsection
+
+@push('scripts')
+<script>
+$(document).ready(function() {
+    // Javascript method's body can be found in assets/assets-for-demo/js/demo.js
+    demo.initChartsPages();
+});
+</script>
+@endpush
