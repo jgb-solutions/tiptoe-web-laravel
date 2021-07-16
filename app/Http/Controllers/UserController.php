af2a51c407b60900f69a548e99a06a0b9b8b2b99
Poliard Djimmy <poliarddjimmy@gmail.com>
2021-07-16 00:39:32 -0400

    Diplay users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,6 +16,7 @@ class UserController extends Controller
      */
     public function index(User $model)
     {
-        return view('users.index', ['users' => $model->paginate(15)]);
+        $users = $model->where('user_type', 'consumer')->paginate(15);
+        return view('users.index', compact('users'));
     }
 }
