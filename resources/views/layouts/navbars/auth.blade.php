4e2671c8fdcb1036454fe283f160d6b8dfca2ce4
Poliard Djimmy <poliarddjimmy@gmail.com>
2021-07-16 00:43:14 -0400

    hotfix

--- resources/views/layouts/navbars/auth.blade.php
+++ resources/views/layouts/navbars/auth.blade.php
@@ -14,7 +14,7 @@
                     <p>{{ __('Dashboard') }}</p>
                 </a>
             </li>
-            <li class="{{ $elementActive == 'user' || $elementActive == 'profile' ? 'active' : '' }}">
+            <li class="{{ $elementActive == 'models' || $elementActive == 'consumers' ? 'active' : '' }}">
                 <a data-toggle="collapse" aria-expanded="false" href="#usersManagement">
                     <i class="nc-icon nc-single-02"></i>
                     <p>
@@ -22,17 +22,17 @@
                         <b class="caret mt-1"></b>
                     </p>
                 </a>
-                <div class="collapse {{ $elementActive == 'user' || $elementActive == 'profile' ? 'show' : 'hide'  }}"
+                <div class="collapse {{ $elementActive == 'models' || $elementActive == 'consumers' ? 'show' : 'hide'  }}"
                     id="usersManagement">
                     <ul class="nav">
-                        <li class="{{ $elementActive == 'profile' ? 'active' : '' }}">
-                            <a href="{{ route('profile.edit') }}">
+                        <li class="{{ $elementActive == 'models' ? 'active' : '' }}">
+                            <a href="{{ route('users', ['type' => 'model']) }}">
                                 <span class="sidebar-mini-icon">{{ __('M') }}</span>
                                 <span class="sidebar-normal">{{ __(' Model') }}</span>
                             </a>
                         </li>
-                        <li class="{{ $elementActive == 'user' ? 'active' : '' }}">
-                            <a href="{{ route('page.index', 'user') }}">
+                        <li class="{{ $elementActive == 'consumers' ? 'active' : '' }}">
+                            <a href="{{ route('users', ['type' => 'consumer']) }}">
                                 <span class="sidebar-mini-icon">{{ __('C') }}</span>
                                 <span class="sidebar-normal">{{ __(' Consumer') }}</span>
                             </a>
@@ -46,30 +46,7 @@
                     <p>{{ __('Icons') }}</p>
                 </a>
             </li>
-            <li class="{{ $elementActive == 'map' ? 'active' : '' }}">
-                <a href="{{ route('page.index', 'map') }}">
-                    <i class="nc-icon nc-pin-3"></i>
-                    <p>{{ __('Maps') }}</p>
-                </a>
-            </li>
-            <li class="{{ $elementActive == 'notifications' ? 'active' : '' }}">
-                <a href="{{ route('page.index', 'notifications') }}">
-                    <i class="nc-icon nc-bell-55"></i>
-                    <p>{{ __('Notifications') }}</p>
-                </a>
-            </li>
-            <li class="{{ $elementActive == 'tables' ? 'active' : '' }}">
-                <a href="{{ route('page.index', 'tables') }}">
-                    <i class="nc-icon nc-tile-56"></i>
-                    <p>{{ __('Table List') }}</p>
-                </a>
-            </li>
-            <li class="{{ $elementActive == 'typography' ? 'active' : '' }}">
-                <a href="{{ route('page.index', 'typography') }}">
-                    <i class="nc-icon nc-caps-small"></i>
-                    <p>{{ __('Typography') }}</p>
-                </a>
-            </li>
+
             <!-- <li class="active-pro {{ $elementActive == 'upgrade' ? 'active' : '' }}">
                 sdwd
             </li> -->
