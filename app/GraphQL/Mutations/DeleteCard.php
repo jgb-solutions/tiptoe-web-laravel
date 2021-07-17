0c7fabd17261503cfbdcac0155df2925fca15cc7
Poliard Djimmy <poliarddjimmy@gmail.com>
2021-07-16 21:39:24 -0400

    Created DeleteCard mutation

---
+++ app/GraphQL/Mutations/DeleteCard.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+class DeleteCard
+{
+    /**
+     * @param  null  $_
+     * @param  array<string, mixed>  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        // TODO implement the resolver
+        extract($args);
+        $success = false;
+
+        $user = auth()->user();
+
+        \DB::beginTransaction();
+        try{
+            $user->findPaymentMethod($card)->delete();
+        }catch(\Exception $e){
+            \DB::rollback();
+            \Log::info('We cannot delete your card at this time. Try agian', ['error' => $e] );
+        }
+        \DB::commit();
+        
+        return ['success'=> $success];
+    }
+}
