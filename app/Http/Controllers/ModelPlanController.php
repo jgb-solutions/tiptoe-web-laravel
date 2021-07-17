941bbff1d57fa494e13071925616af911e74e8c2
Poliard Djimmy <poliarddjimmy@gmail.com>
2021-07-16 21:40:33 -0400

    Update ModelPlanController

--- app/Http/Controllers/ModelPlanController.php
+++ app/Http/Controllers/ModelPlanController.php
@@ -4,15 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Models\ModelPlan;
 use Illuminate\Http\Request;
-use Stripe;
+use \Stripe\Stripe;
 
 class ModelPlanController extends Controller
 {
-    protected $stripeId;
+    protected $stripe;
 
     public function __construct()
     {
-        $this->stripeId = Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
+        Stripe::setApiKey(config('services.stripe.secret'));
+        $this->stripe = new \Stripe\StripeClient(env('STRIPE_SECRET'));
+        
     }
     
     /**
@@ -41,30 +43,87 @@ class ModelPlanController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
+    
     public function store(Request $request)
     {
         $data = $request->except('_token');
 
-        $data['slug'] = strtolower($data['name']);
+        // $data['name'] = "Grace";
+        // $data['cost'] = 100;
+        // $data['user_id'] = 4;
 
         //create stripe product
         $stripeProduct = $this->stripe->products->create([
             'name' => $data['name'],
         ]);
-        
-        //Stripe Plan Creation
-        $stripe->prices->create([
-            'unit_amount' => $data['cost'] * 100,
-            'currency' => 'usd',
-            'recurring' => ['interval' => 'month'],
-            'product' => $stripeProduct->id,
-          ]);
+    
+        \DB::beginTransaction();
+        try{
+            //Stripe Plan Creation
+            $stripePlanCreation = $this->stripe->prices->create([
+                'unit_amount' => $data['cost'] * 100,
+                'currency' => 'usd',
+                'recurring' => ['interval' => 'month'],
+                'product' => $stripeProduct->id,
+            ]);
 
-        $data['stripe_plan'] = $stripePlanCreation->id;
-
-        Plan::create($data);
+            ModelPlan::create([
+                'name' => $data['name'],
+                'user_id' => $data['user_id'],
+                'stripe_plan' => $stripePlanCreation->id,
+                'cost' => $data['cost']
+            ]); 
+        }catch(\Exception $e){
+            \DB::rollback();
+            \Log::info('The price cannot be create at this time', ['error' => $e] );
+            return back()->with('warning','The price cannot be create at this time');
+        }
+        \DB::commit();
 
         echo 'plan has been created';
+    }
+
+    
+    
+    public function AddPrice(Request $request)
+    {
+        $data = $request->except('_token');
+
+        return $modeles = auth()->user()->modeles->where('id', 1)->first();
+        
+        
+        $product = $this->stripe->prices->retrieve( $data['stripePrice_id'], [] );
+        
+        $price = ModelPlan::where('user_id', $data['user_id'])->first();
+        
+        if($price->cost ==  $data['cost'])
+        {
+            return "This price is alredy there";
+        }
+        else{
+            \DB::beginTransaction();
+            try{
+                $stripePlanCreation = $this->stripe->prices->create([
+                    'unit_amount' => $data['cost'] * 100,
+                    'currency' => 'usd',
+                    'recurring' => ['interval' => 'month'],
+                    'product' => $product->product,
+                ]);
+                
+                $price->update([
+                    'cost' => $data['cost'],
+                    'stripe_plan' => $stripePlanCreation->id,
+                ]);
+                
+            }catch(\Exception $e){
+                \DB::rollback();
+                \Log::info('The price cannot be create at this time', ['error' => $e] );
+                return back()->with('warning','The price cannot be create at this time');
+            }
+            \DB::commit();
+        }
+
+        echo 'The plan has been changed';
     }
 
     /**
