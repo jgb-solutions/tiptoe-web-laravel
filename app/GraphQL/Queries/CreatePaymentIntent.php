dd5e0093551f2c60b6a5ce33ec2a6d0dbdbbd8a8
Poliard Djimmy <poliarddjimmy@gmail.com>
2021-07-22 18:42:28 -0400

    create CreatePaymentIntent

---
+++ app/GraphQL/Queries/CreatePaymentIntent.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\GraphQL\Queries;
+
+// use \Stripe\Stripe;
+
+class CreatePaymentIntent
+{
+    /**
+     * @param  null  $_
+     * @param  array<string, mixed>  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        // TODO implement the resolver
+        $data = ["client_secret" => auth()->user()->createSetupIntent()->client_secret];
+        return $data;
+    }
+}
