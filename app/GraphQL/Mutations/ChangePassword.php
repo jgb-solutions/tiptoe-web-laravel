3382102eda76c7b6ee389f95cc0fabf93035e499
Poliard Djimmy <poliarddjimmy@gmail.com>
2021-07-28 23:07:16 -0400

    adde changePassword mutation

---
+++ app/GraphQL/Mutations/ChangePassword.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+use Illuminate\Support\Facades\Hash;
+
+class ChangePassword
+{
+    /**
+     * @param  null  $_
+     * @param  array<string, mixed>  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        // TODO implement the resolver
+        extract($args);
+
+        $response = array();
+
+        if(Hash::check($password, auth()->user()->password))
+        {
+            if (auth()->user()->update(["password" => Hash::make($new_password)])) {
+                $response = ["success"=> true, "message"=> "Your password has been changed"];
+            } else {
+                $response = ["success"=> false, "message"=> "Your password was not changed"];
+            }
+        }
+        else
+        {
+            $response = ["success"=> false, "message"=> "Incorrect password"];
+        }
+
+        return ["response" => $response];
+    }
+}
